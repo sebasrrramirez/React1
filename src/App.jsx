c9c67044de81e041f949831e5c8e43a861236fab
Sebas <sebastianramirezmdz@gmail.com>
2024-01-02 20:36:13 -0300

    ultima

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero.jsx";
-import CardProducts from "./components/CardsProducts/CardProducts.jsx";
-import Footer from "./components/Footer/Footer.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Hero from "./Components/Hero/Hero.jsx";
+import CardProducts from "./Components/CardsProducts/CardProducts.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
 import "./App.css";
 
 export default function App() {
